b16414499d69b6b3465c95e205cace1f7bda3ecb
SplitWolf <mat.jake1360@gmail.com>
2023-12-31 14:14:09 -0500

    feat(timegrid): Use date specific colors and custom color for ASel

--- src/time_grid.rs
+++ src/time_grid.rs
@@ -1,9 +1,7 @@
-use chrono::DateTime;
-use chrono::Local;
-use chrono::NaiveTime;
+use std::{fmt, cmp};
+use std::collections::HashMap;
+use chrono::{ DateTime, Local, NaiveDate, NaiveTime, Duration, Datelike};
 use leptos::*;
-use std::fmt;
-use std::cmp;
 
 #[derive(PartialEq,Clone, Copy)]
 pub enum SelectionMode  {
@@ -16,15 +14,17 @@ pub enum HighlightColor {
     Red,
     Green,
     Yellow,
+    Select,
     None
 }
 impl fmt::Display for HighlightColor {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            HighlightColor::Red => write!(f, "highlight-red"),
+            HighlightColor::Red =>    write!(f, "highlight-red"),
             HighlightColor::Yellow => write!(f, "highlight-yellow"),
-            HighlightColor::Green => write!(f, "highlight-green"),
-            HighlightColor::None => write!(f, "")
+            HighlightColor::Green =>  write!(f, "highlight-green"),
+            HighlightColor::Select => write!(f, "highlight-select"),
+            HighlightColor::None =>   write!(f, "")
         }
     }
 }
@@ -33,14 +33,13 @@ struct TimeSlot {
     id: u32,
     start_time: NaiveTime,
     end_time: NaiveTime,
-    dayColors: Vec<DayColor>,
-    color: RwSignal<HighlightColor>,
+    day_colors: RwSignal<HashMap<NaiveDate, HighlightColor>>,
     weekend: bool
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 struct DayColor {
-    date: DateTime<Local>,
+    date: NaiveDate,
     color: HighlightColor
 }
 
@@ -50,6 +49,9 @@ pub fn TimeGrid(select_mode: ReadSignal<SelectionMode>, select_color: ReadSignal
     let (timeslots, set_timeslots) = create_signal(vec![]);
     let (select_current,set_select_current) = create_signal(0);
     let time_calc = vec!["06","07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22" ];
+    let weekdate = move |x: u32| {
+    curr_date().date_naive().checked_add_signed(Duration::days((i64::from(x)-i64::from(curr_date().weekday().num_days_from_sunday())).into())).unwrap()};
+
 
     for i in 0..=230 {
         let index = if i >= 7 {((i+7)/7)/2} else {0} as usize;
@@ -58,27 +60,36 @@ pub fn TimeGrid(select_mode: ReadSignal<SelectionMode>, select_color: ReadSignal
                 id: i,
                 start_time: NaiveTime::from_hms_opt(time_calc[index].parse::<u32>().unwrap(),if (i/7)%2==1 { 0 } else { 30 },0).expect("HARDCODED VALUE: Time Start"),
                 end_time: NaiveTime::from_hms_opt(time_calc[index].parse::<u32>().unwrap(),if (i/7)%2==1 { 0 } else { 30 },0).expect("HARDCODED VALUE: Time End"),
-                dayColors: vec![],
-                color: create_rw_signal(HighlightColor::None),
+                day_colors: create_rw_signal(HashMap::new()),
                 weekend: i % 7 == 0 || i % 7 == 6
             })});
     }
 
     let singleSelect = move |child_id: u32, set_color: HighlightColor| {
         timeslots.with(|slots| {
-            slots.into_iter().filter(|box_div| box_div.id == child_id).for_each(|div| {
+            slots.into_iter().filter(|box_div| box_div.id == child_id).for_each(|slot| {
                 // logging::log!("{}",div.start_time);
-                div.color.update(move |color| {
-                    if *color != set_color {
-                        *color = set_color;
+                // slot.color.update(move |color| {
+                //     if *color != set_color {
+                //         *color = set_color;
+                //     } else {
+                //         *color = HighlightColor::None;
+                //     }
+                // });
+                //TODO: Make this only use the day_colors signal syncing the state with out a derived signal is impossible, 
+                // this or make it a derived signal with condition for other colors
+                slot.day_colors.update(move |colors| {
+                    if colors.get(&weekdate(child_id%7)).unwrap_or(&HighlightColor::None) != &set_color {
+                        colors.insert(weekdate(child_id%7),set_color);
                     } else {
-                        *color = HighlightColor::None;
+                        colors.insert(weekdate(child_id%7),HighlightColor::None);
                     }
                 });
+                
             });
         })
     };
-    
+
     let areaSelect = move |child_id: u32, set_color: HighlightColor| {
         let row_begin =move || cmp::min(child_id/7,(select_current()-1)/7);
         let row_end =move ||  cmp::max(child_id/7,(select_current()-1)/7);
@@ -87,16 +98,21 @@ pub fn TimeGrid(select_mode: ReadSignal<SelectionMode>, select_color: ReadSignal
         timeslots.with(|slots| {
             slots.into_iter().filter(|box_div| 
                 row_begin() <= box_div.id/7 && box_div.id/7 <= row_end() 
-                && col_begin() <= box_div.id%7 && box_div.id%7 <= col_end() ).for_each(|div| {
-                div.color.update(|color| {
-                    *color = set_color;
-                });
+                && col_begin() <= box_div.id%7 && box_div.id%7 <= col_end() ).for_each(|slot| {
+                    slot.day_colors.update(move |colors| {
+                        colors.insert(weekdate(slot.id%7),set_color);
+                        // if colors.get(&weekdate(child_id%7)).unwrap_or(&HighlightColor::None) != &set_color {
+                        // } else {
+                        //     colors.insert(weekdate(child_id%7),HighlightColor::None);
+                        // }
+                    });
             });
             set_select_current.update(|value| {
                 *value = 0;
             });
         })
     };
+
     
     //TODO: Use box divs signal to create effect and store data when sumbit btn clicked
     
@@ -120,9 +136,18 @@ pub fn TimeGrid(select_mode: ReadSignal<SelectionMode>, select_color: ReadSignal
             <div 
             class="box" 
             class:weekend={ move || child.weekend}
-            class:highlight-red={move || child.color.get() == HighlightColor::Red} 
-            class:highlight-yellow={move || child.color.get() == HighlightColor::Yellow} 
-            class:highlight-green={move || child.color.get() == HighlightColor::Green} 
+            class:highlight-red={move || 
+                (child.day_colors)().get(&weekdate(child.id%7)).unwrap_or(&HighlightColor::None) == &HighlightColor::Red
+            }
+            class:highlight-yellow={move || 
+                (child.day_colors)().get(&weekdate(child.id%7)).unwrap_or(&HighlightColor::None) == &HighlightColor::Yellow
+            }
+            class:highlight-green={move || 
+                (child.day_colors)().get(&weekdate(child.id%7)).unwrap_or(&HighlightColor::None) == &HighlightColor::Green
+            }
+            class:highlight-select={
+                move || (child.day_colors)().get(&weekdate(child.id%7)).unwrap_or(&HighlightColor::None) == &HighlightColor::Select
+            }
             on:click=move |_| {
                 match (move || select_mode())() {
                     SelectionMode::Single => {
@@ -130,19 +155,7 @@ pub fn TimeGrid(select_mode: ReadSignal<SelectionMode>, select_color: ReadSignal
                     },
                     SelectionMode::AreaSelect => {
                         if (move || select_current() == 0)() {
-                            //TODO: Replace with custom highlight color single select
-                            timeslots.with(|boxes| { 
-                                boxes.into_iter().filter(|box_div| box_div.id == child.id).for_each(|div| {
-                                    div.color.update(|color| {
-                                        //TODO: Update how corner box is colored
-                                        if *color == HighlightColor::None {
-                                            *color = select_color();
-                                        } else {
-                                            *color = HighlightColor::None;
-                                        }
-                                    });
-                                });
-                            });
+                            singleSelect(child.id, HighlightColor::Select);
                             set_select_current.update(|value| {
                                 *value = child.id+1;
                             });
@@ -152,14 +165,7 @@ pub fn TimeGrid(select_mode: ReadSignal<SelectionMode>, select_color: ReadSignal
                     },
                     SelectionMode::AreaDeselect => {
                         if (move || select_current() == 0)() {
-                            //TODO: Replace with custom highlight color single select
-                            timeslots.with(|boxes| {
-                                boxes.into_iter().filter(|box_div| box_div.id == child.id).for_each(|div| {
-                                    div.color.update(|color| {
-                                        *color = select_color()
-                                    });
-                                });
-                            });
+                            singleSelect(child.id, HighlightColor::Select);
                             set_select_current.update(|value| {
                                 *value = child.id+1;
                             });
